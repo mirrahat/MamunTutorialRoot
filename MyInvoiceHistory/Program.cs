7c2b9de9b87e7ddfd354d8efc64989b7578772cb
mirrahat <mirrahatfiverr@gmail.com>
2025-02-13 09:10:46 +0600

    update history

--- MyInvoiceHistory/Program.cs
+++ MyInvoiceHistory/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.MyInvoiceHistory.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
