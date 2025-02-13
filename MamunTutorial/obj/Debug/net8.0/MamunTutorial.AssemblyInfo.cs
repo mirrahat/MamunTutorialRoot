b6eba6769c0bb00ccac7e75fdfcdbf7a0ec62247
mirrahat <mirrahatfiverr@gmail.com>
2025-02-13 08:35:01 +0600

    update

--- MamunTutorial/obj/Debug/net8.0/MamunTutorial.AssemblyInfo.cs
+++ MamunTutorial/obj/Debug/net8.0/MamunTutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a7888879403410c779fe5c04dfbe8a2c705819")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+351b56282108cb74a9a636823382a662edd975c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
