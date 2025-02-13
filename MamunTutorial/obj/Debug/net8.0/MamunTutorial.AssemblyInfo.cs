ab00841eda3fda4f876570bf42c2a9edd75b92b4
mirrahat <mirrahatfiverr@gmail.com>
2025-02-13 08:05:58 +0600

    Removed AI MATH GENERATOR SHAT project reference

--- MamunTutorial/obj/Debug/net8.0/MamunTutorial.AssemblyInfo.cs
+++ MamunTutorial/obj/Debug/net8.0/MamunTutorial.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34e6925824f61289ed26b39fdb52dde6cdae88cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f727ffe93fdc2d6dcbf3b62763782ea8a0faf42")]
 [assembly: System.Reflection.AssemblyProductAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MamunTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
